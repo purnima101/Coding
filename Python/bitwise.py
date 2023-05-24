2cce6299ce1d6f609dfad2290d275c8fbe293a4c
Purnima Agarwal <37765473+purnima101@users.noreply.github.com>
2023-05-24 12:03:01 +0530

    bitwise

---
+++ Python/bitwise.py
@@ -0,0 +1,27 @@
+ans=0
+i=0
+
+# decimal to binary
+
+# n=10000
+# while(n!=0):
+#     b=n&1
+#     n=n>>1
+#     ans=(pow(10,i)*b)+ans
+#     i=i+1
+
+# print(ans)
+
+# binary to decimal
+
+n=10101
+i=0
+ans=0
+while(n!=0):
+    b=n%10
+    ans=ans+(b*pow(2,i))
+    i=i+1
+    n=n//10
+
+print(ans)
+
