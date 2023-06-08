40da435cca009f2e9914274992e5db1840154ce5
Purnima Agarwal <37765473+purnima101@users.noreply.github.com>
2023-06-08 19:21:14 +0530

    push

---
+++ Java/Alternate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Alternate {
+    public static void main(String[] args)
+    {
+ 
+        // Here array is the
+        // array name of int type
+        int[] array = new int[4];
+ 
+        System.out.println("The size of "
+                        + "the array is "
+                        + array.length);
+    }
+}
