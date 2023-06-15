9e92c926442b4ed396327af2a162a28a7b9aa0fa
Purnima <purnimaagarwal2013@gmail.com>
2023-06-15 15:38:40 +0530

    commit

--- Java/UniqueNoOfOccurance.java
+++ Java/UniqueNoOfOccurance.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class UniqueNoOfOccurance {
     public static void main(String[] args)  {
-        int[] arr = new int[] {1,1,2,3,3,2,2,2};
+        int[] arr = new int[] {1,2,3,3,2,2,2};
         HashMap<Integer, Integer> mp = new HashMap<Integer, Integer>();
         int i;
         for (i = 0; i < arr.length; i++) {
