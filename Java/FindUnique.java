2a66b4e4e0eef74ebb2e1194f634588fe173c375
Purnima <purnimaagarwal2013@gmail.com>
2023-06-15 12:02:06 +0530

    commit

--- Java/FindUnique.java
+++ Java/FindUnique.java
@@ -1,8 +1,14 @@
 import java.util.*;
-public class FindUnique {
-    public static void main(String[] args){
-        int[] arr= new int[]{1,3,4,1,3};
-        
 
+import java.util.*;
+
+public class FindUnique {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 3, 4, 1, 3,4,7 };
+        int i, a=arr[0];
+        for(i = 1; i < arr.length; i++){
+            a=a^arr[i];
+        }
+        System.out.println(a);
     }
 }
