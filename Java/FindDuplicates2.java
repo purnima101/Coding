2d021327021a73893e82c96167ed77a035afa89e
Purnima <purnimaagarwal2013@gmail.com>
2023-06-19 15:28:50 +0530

    commit

--- Java/FindDuplicates2.java
+++ Java/FindDuplicates2.java
@@ -2,7 +2,14 @@ import java.util.*;
 
 public class FindDuplicates2 {
     public static void main(String[] args) {
-        int[] nums = new int[] { 1, 2, 4, 4, 1 };
+        int[] nums = new int[] { 4,3,2,7,8,2,3,1 };
+        int ans=0;
+        int i;
+        for(i=0;i<nums.length;i++)
+        {
+            ans=ans^nums[i];
+        }
 
+System.out.println(ans);
     }
 }
---
+++ Java/IntersectionOfArray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class IntersectionOfArray {
+    public static void main(String[] args){
+        int[] arr1=new int[]{1,4,5};
+        int[] arr2=new int[]{2,4,5 };
+        ArrayList<Integer> arr=new  ArrayList<Integer>();
+        int i=0,j=0;
+         while(i<arr1.length && j<arr2.length){
+            
+
+    }
+    
+}
