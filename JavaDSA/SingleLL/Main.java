29055afffdc7216a93d5489a45f789f9a207bb3d
Purnima Agarwal <37765473+purnima101@users.noreply.github.com>
2023-07-03 11:10:47 +0530

    commit

--- JavaDSA/SingleLL/Main.java
+++ JavaDSA/SingleLL/Main.java
@@ -1,5 +1,9 @@
 package JavaDSA.SingleLL;
 import java.util.*;
 public class Main {
-    
+    public static void main(String args[]) {
+        SingleLinkedList sll=new SingleLinkedList();
+        sll.createNode(23);
+        System.out.println(sll.head.next.value);
+    }
 }
--- JavaDSA/SingleLL/SingleLinkedList.java
+++ JavaDSA/SingleLL/SingleLinkedList.java
@@ -1,5 +1,25 @@
 package JavaDSA.SingleLL;
 import java.util.*;
 public class SingleLinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
     
+    public Node createNode(int nodeValue){
+        head=new Node();
+        tail=new Node();
+        Node node=new Node();
+        node.value=nodeValue;
+        node.next=null;
+        head.next=node;
+        tail.next=node;
+        size=1;
+        return head;
+    }
+
+    public void insertNode(int nodeValue, int location)
+    {
+        
+    }
+
 }
