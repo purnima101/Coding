0bb88459199f4f56eaa892d3a9e6012be61b737e
Purnima Agarwal <37765473+purnima101@users.noreply.github.com>
2024-05-31 17:23:59 +0530

    commit

---
+++ Python/binarySearchRec.py
@@ -0,0 +1,19 @@
+def search(arr, l,m,mid, key):
+    if arr[mid]==key:
+        return True
+    if l==m:
+        return False
+    if(arr[mid]>key):
+        
+    
+
+def main():
+    arr=[1,4,7,9,10,7]
+    key=12
+    l=0
+    m=len(arr)-1
+    mid=l+m//2
+
+    print(search(arr,l,m,mid, key))
+
+main()
